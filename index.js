063d22e8df22c7a43f8dbf1617009e4654d63e68
Zac Barreca <zacbarreca@gmail.com>
2020-02-20 11:45:44 -0500

    initialize index files

---
+++ index.js
@@ -0,0 +1,17 @@
+function getDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
